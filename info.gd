26157acc99a4538753c4aa425c5a729bf06bf104
thZero.com <development@thzero.com>
2019-03-04 20:36:22 -0600

    'about'

--- info.gd
+++ info.gd
@@ -12,12 +12,71 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-const authors = """
-"""
+const team = {
+	"PROJECT_FOUNDERS": 
+	{
+		"Members": {
+			"1": {
+				"Name": "Bob"
+			},
+			"2": {
+				"Name": "Sam"
+			}
+		}
+	},
+	"LEAD_DEVELOPERS": 
+	{
+		"Members": {
+			"1": {
+				"Name": "Fred"
+			},
+			"2": {
+				"Name": "Irina"
+			}
+		}
+	},
+	"PROJECT_MANAGERS": 
+	{
+		"Members": {
+			"1": {
+				"Name": "Bob"
+			}
+		}
+	},
+	"DEVELOPERS": 
+	{
+		"Members": {
+			"1": {
+				"Name": "Mary"
+			},
+			"2": {
+				"Name": "Todd"
+			},
+			"3": {
+				"Name": "Luke"
+			}
+		}
+	},
+	"BACKERS": 
+	{
+		"Members": {
+			"1": {
+				"Name": "Savage Hand"
+			},
+			"2": {
+				"Name": "Bloody Mary"
+			},
+			"3": {
+				"Name": "Tim Toolman"
+			}
+		}
+	}
+}
 
 const license_thirdparty = {
 	"Godot": 
 	{
+		"Name": "Godot",
 		"License": """
 """,
 		"Type": "MIT",
@@ -25,6 +84,7 @@ const license_thirdparty = {
 	},
 	"Test1": 
 	{
+		"Name": "Test1",
 		"License": """
 """,
 		"Type": "BSD",
@@ -32,6 +92,7 @@ const license_thirdparty = {
 	},
 	"Test2":
 	{
+		"Name": "Test2",
 		"License": """
 """,
 		"Type": "Apache 2.0",
--- main_menu/about.gd
+++ main_menu/about.gd
@@ -2,12 +2,55 @@ extends Control
 
 signal about_finished()
 
+const line_break = "\n"
+const tab = "\t"
+
 var _fsm
 var _info
+var _tree
+var _tree_label
+var _tree_all
+var _license_all = ""
 
 func _on_button_ok_pressed():
 	emit_signal("about_finished")
+
+func _on_tree_license_third_party_item_selected():
+	_tree_label.clear()
 	
+	var selected = _tree.get_selected()
+	if (selected == null):
+		return
+	
+	print(selected.get_text(0))
+	
+	if (selected == _tree_all):
+		_tree_label.add_text(_license_all)
+		return
+	
+	var license
+	for i in _info.license_thirdparty:
+		license = _info.license_thirdparty[i]
+		if (selected != license.node):
+			continue
+		
+		_tree_label.clear()
+		_tree_label.add_text(license.output)
+
+func _generate_output(license):
+	var output = license.Name + line_break
+	if (license.Url != null && license.Url != ""):
+		output += tab + tr("MAIN_ABOUT_LICENSE_THIRD_PARTY_REPOSITORY") + ": " + license.Url + line_break
+	if (license.License != null && license.License != ""):
+		if (output.length() > 0):
+			output += line_break
+		output += license.License + "\n"
+	if (license.Type != null && license.Type != ""):
+		if (output.length() > 0):
+			output += line_break
+		output += tab + tr("MAIN_ABOUT_LICENSE_THIRD_PARTY_LICENSE") + ": " + license.Type
+	return output
+
 #### State
 func _on_state_changed(state_from, state_to, args):
 	_fsm._print_state(state_from, state_to, args)
@@ -16,10 +59,10 @@ func _ready():
 	_fsm = state.new()
 	_fsm.initialize(self)
 	
-	_info = load("res://about.gd")
+	_info = load("res://info.gd")
 	
 	var tabs = find_node("tabs")
-	tabs.set_tab_title(0, tr("MAIN_ABOUT_AUTHORS"))
+	tabs.set_tab_title(0, tr("MAIN_ABOUT_TEAM"))
 	tabs.set_tab_title(1, tr("MAIN_ABOUT_LICENSE"))
 	tabs.set_tab_title(2, tr("MAIN_ABOUT_LICENSE_THIRD_PARTY"))
 	
@@ -27,8 +70,56 @@ func _ready():
 	find_node("label_copyright").add_text("sdfsdf\n")
 	find_node("label_copyright").add_text("werwer")
 	
-	find_node("label_authors").set_text("sdfsdfsdf")
 	find_node("label_license").set_text(_info.license)
+	
+	var description = tr("MAIN_ABOUT_LICENSE_THIRD_PARTY_DESCRIPTION")
+	description = description % [tr("TITLE"), tr("TITLE")]
+	find_node("label_license_third_party_description").add_text(description)
+	
+	_tree_label = find_node("label_license_third_party")
+	_tree = find_node("tree_license_third_party")
+	
+	var tree_root = _tree.create_item()
+	tree_root.set_text(0, 'none')
+	_tree.set_hide_root(true)
+	
+	_tree_all = _tree.create_item()
+	_tree_all.set_text(0, tr('MAIN_ABOUT_LICENSE_THIRD_PARTY_ALL'))
+	var tree_components = _tree.create_item()
+	tree_components.set_text(0, tr('MAIN_ABOUT_LICENSE_THIRD_PARTY_COMPONENTS'))
+	
+	_license_all = ""
+	var license
+	var child
+	for i in _info.license_thirdparty:
+		license = _info.license_thirdparty[i]
+		child = _tree.create_item(tree_components)
+		child.set_text(0, license.Name)
+		license["node"] = child
+		
+		license["output"] = _generate_output(license)
+		_license_all += license["output"]
+		_license_all += line_break + line_break
+	
+	var output = ""
+	var group
+	var members
+	var member
+	for i in _info.team:
+		output += tr("MAIN_ABOUT_TEAM_" + i) + line_break + line_break
+		group = _info.team[i]
+		if (group == null):
+			continue
+		members = group.Members
+		if (members == null):
+			continue
+		for j in members:
+			member = members[j]
+			output += tab + member.Name + line_break
+		
+		output += line_break + line_break
+		
+	find_node("label_team").set_text(output)
 	
 	find_node("button_ok").set_text(tr("BUTTON_OK"))
 
