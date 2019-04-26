dc113872d1d52b97209c6f67e796ea96271cd924
thZero.com <development@thzero.com>
2019-04-25 21:07:00 -0500

    cleanup of commented code

--- game/wall.gd
+++ game/wall.gd
@@ -6,26 +6,3 @@ export var side = enums.SIDES.left
 
 func is_scoreable():
 	return ((side == enums.SIDES.left) || (side == enums.SIDES.right))
-
-#func _on_wall_area_entered(area):
-#	if area.get_name() != "ball":
-#		return
-#
-#	if (!get_tree().is_network_server()):
-#		return
-#
-#	_wall_floor_ceiling(area)
-#	_wall_left_right(area)
-#
-#func _wall_floor_ceiling(area):
-#	if (!((side == enums.SIDES.top) || (side == enums.SIDES.bottom))):
-#		return
-#
-#	area.rpc("bounce_side", side)
-#
-#func _wall_left_right(area):
-#	if (!((side == enums.SIDES.left) || (side == enums.SIDES.right))):
-#		return
-#
-#	area.rpc("reset", side)
-#	get_parent().rpc("score", side)
